dbc787ad79b31710a8808fc7111d677159efafbf
Francesco Esposito <francesco7.esposito@gmail.com>
2025-01-25 02:26:26 +0100

    Add packages

---
+++ Assets/Unit.cs
@@ -0,0 +1,13 @@
+using UnityEngine;using UnityEngine.EventSystems;
+
+public class Unit : MonoBehaviour, IPointerClickHandler
+{
+    public void MoveInput(Vector2[] inputDirections)
+    {
+        //transform.SetLocalPositionAndRotation( , Quaternion.identity);
+    }
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        //eventData.
+    }
+}
