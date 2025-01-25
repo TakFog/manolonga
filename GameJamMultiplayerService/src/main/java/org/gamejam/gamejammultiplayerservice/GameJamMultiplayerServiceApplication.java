89a73f27048379e7d0494e7c344284be60672d69
lorenzopatane <lorenzo.patane@lansweeper.com>
2025-01-25 04:42:25 +0100

    Initial barebone server

---
+++ GameJamMultiplayerService/src/main/java/org/gamejam/gamejammultiplayerservice/GameJamMultiplayerServiceApplication.java
@@ -0,0 +1,32 @@
+package org.gamejam.gamejammultiplayerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@SpringBootApplication
+@RestController
+public class GameJamMultiplayerServiceApplication {
+    private final Map<Integer, Map<String, String>> stateByPlayer = new ConcurrentHashMap<>();
+
+    @GetMapping("/ping")
+    String ping() {
+        return "pong";
+    }
+
+    @PostMapping("/updateState/{playerId}/{roundId}")
+    Map<String, String> updateState(@PathVariable String playerId, @PathVariable final int roundId, @RequestBody String statePayload) {
+        Map<String, String> stringStringMap = stateByPlayer.computeIfAbsent(roundId, k -> new ConcurrentHashMap<>());
+        stringStringMap.put(playerId, statePayload);
+        stateByPlayer.remove(roundId - 2); // Clean up old states
+        stateByPlayer.keySet().removeIf(k -> k > roundId); // Cleanup future states if we are behind (eg a new game started)
+        return stringStringMap;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameJamMultiplayerServiceApplication.class, args);
+    }
+}
---
+++ GameJamMultiplayerService/src/test/java/org/gamejam/gamejammultiplayerservice/GameJamMultiplayerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gamejam.gamejammultiplayerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameJamMultiplayerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
