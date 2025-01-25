4ca82720d32d47146b692672217ec98c6dcb0c23
Francesco Esposito <francesco7.esposito@gmail.com>
2025-01-25 16:36:21 +0100

    WIP

--- Assets/_Scripts/States/WaitForExecutionState.cs
+++ Assets/_Scripts/States/WaitForExecutionState.cs
@@ -1,14 +1,27 @@
+using System.Collections;
 using UnityEngine;
 
 public class WaitForExecutionState : State
 {
-    private Choice childChoice;
-    private Choice monsterChoice;
-    
+    private CommunicationData communicationsOfThisRound;
+
+    public WaitForExecutionState(CommunicationData communicationsOfThisRound)
+    {
+        this.communicationsOfThisRound = communicationsOfThisRound;
+    }
     public override void Enter()
     {
         base.Enter();
+        //Execute
+        StateManager.Instance.NextRound();
+        StateManager.Instance.ChangeState(new WaitForActionChoiceState());
     }
+
+    IEnumerator C_Execute()
+    {
+        
+    }
+    
     public override void Exit()
     {
         base.Exit();
