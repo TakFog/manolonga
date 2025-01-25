e1abf3bebb43fe1eea88b02ff45c1188a4a2fdd0
lorenzopatane <lorenzo.patane@lansweeper.com>
2025-01-25 16:57:29 +0100

    Cleanup

--- Assets/_Scripts/States/WaitForOpponentChoiceReceivedState.cs
+++ Assets/_Scripts/States/WaitForOpponentChoiceReceivedState.cs
@@ -6,38 +6,13 @@ public class WaitForOpponentChoiceReceivedState : State
     public override void Enter()
     {
         base.Enter();
-        CommunicationManager.Instance.OnMovesReceived += MovesReceived;
     }
     void MovesReceived(CommunicationData communicationData)
     {
-        GameObject lightObject = GameObject.Find("Light 2D");
-
-        if (lightObject != null)
-        {
-            // Get the Light2D component
-            Light2D light2D = lightObject.GetComponent<Light2D>();
-
-            if (light2D != null)
-            {
-                // Decrease the Outer Radius by 1
-                light2D.pointLightOuterRadius -= 1;
-
-                Debug.Log("Updated Outer Radius: " + light2D.pointLightOuterRadius);
-            }
-            else
-            {
-                Debug.LogError("Light2D component not found on GameObject!");
-            }
-        }
-        else
-        {
-            Debug.LogError("GameObject 'Light 2D' not found!");
-        }
         StateManager.Instance.ChangeState(new WaitForExecutionState());
     }
     public override void Exit()
     {
         base.Exit();
-        CommunicationManager.Instance.OnMovesReceived -= MovesReceived;
     }
 }
