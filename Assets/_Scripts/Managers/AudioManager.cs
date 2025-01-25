15091ca59c09c76957683af3cdf240e3285603c4
takfog <takfog@gmail.com>
2025-01-25 21:52:49 +0100

    audio manager

---
+++ Assets/_Scripts/Managers/AudioManager.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioSource audioSourceLow;
+    public AudioSource audioSourceMid;
+    public AudioSource audioSourceHigh;
+    
+    public AudioClip[] audioClipsLow;
+    public AudioClip[] audioClipsMid;
+    public AudioClip[] audioClipsHigh;
+
+    public float maxDistLow;
+    public float distLowToMid;
+    public float maxDistMid;
+    public float distMidToHigh;
+    
+    private void Start()
+    {
+        StartCoroutine(C_PlayAllSequence());
+    }
+
+    void OnEnable()
+    {
+        StateManager.Instance.OnRoundCompleted += UpdateDistance;
+    }
+
+    void OnDisable()
+    {
+        StateManager.Instance.OnRoundCompleted -= UpdateDistance;
+    }
+
+    IEnumerator C_PlayAllSequence()
+    {
+        for (int i = 0; i < audioClipsLow.Length; i++)
+        {
+            audioSourceLow.PlayOneShot(audioClipsLow[i]);
+            audioSourceMid.PlayOneShot(audioClipsMid[i]);
+            audioSourceHigh.PlayOneShot(audioClipsHigh[i]);
+            yield return new WaitForSeconds(audioClipsLow[i].length);
+        }
+        StartCoroutine(C_PlayAllSequence());
+    }
+
+    public void UpdateDistance()
+    {
+        var distance = Vector3.Distance(Globals.Monster.transform.position, Globals.Child.transform.position);
+        Debug.Log(distance);
+        
+        if (distance > maxDistLow)
+        {
+            audioSourceLow.volume = 1f;
+            audioSourceMid.volume = 0f;
+            audioSourceHigh.volume = 0f;
+        } 
+        else if (distance > distLowToMid)
+        {
+            var frac = FractionBetween(distance, distLowToMid, maxDistLow);
+            audioSourceLow.volume = frac;
+            audioSourceMid.volume = 1 - frac;
+            audioSourceHigh.volume = 0f;
+        }
+        else if (distance > maxDistMid)
+        {
+            audioSourceLow.volume = 0f;
+            audioSourceMid.volume = 1f;
+            audioSourceHigh.volume = 0f;
+        } 
+        else if (distance > distMidToHigh)
+        {
+            var frac = FractionBetween(distance, distMidToHigh, maxDistMid);
+            audioSourceLow.volume = 0f;
+            audioSourceMid.volume = frac;
+            audioSourceHigh.volume = 1 - frac;
+        }
+        else
+        {
+            audioSourceLow.volume = 0f;
+            audioSourceMid.volume = 0f;
+            audioSourceHigh.volume = 1f;
+        } 
+
+    }
+
+    private float FractionBetween(float value, float min, float max)
+    {
+        return (value - min) / (max - min);
+    }
+
+}
