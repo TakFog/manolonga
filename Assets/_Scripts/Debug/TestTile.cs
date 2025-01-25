fb349573a3f9444edcfbb0d54b9daaf8be69e5d5
takfog <takfog@gmail.com>
2025-01-25 12:52:25 +0100

    tiles at distance

---
+++ Assets/_Scripts/Debug/TestTile.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TestTile : MonoBehaviour
+{
+    public GameObject go;
+    public Vector3Int cell;
+    public int distance;
+    public Transform parent;
+
+    [ContextMenu("WorldToTile")]
+    public void WorldToTile()
+    {
+        var pos = TilemapManager.Instance.GetCellAtWorldPosition(go.transform.position);
+        Debug.Log(pos);
+    }
+
+    [ContextMenu("Near")]
+    public void Near()
+    {
+        foreach (var point in TilemapManager.Instance.GetNearCellsWorldPoints(cell, distance))
+        {
+            Instantiate(go, point, Quaternion.identity, parent);
+        }
+    }
+}
---
+++ Assets/_Scripts/TilemapManager.cs
@@ -0,0 +1,100 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TilemapManager : MonoBehaviour
+{
+    public static TilemapManager Instance;
+    
+    private Tilemap tilemap;
+    
+    private void Awake()
+    {
+        Instance = this;
+        if (tilemap == null)
+            tilemap = FindAnyObjectByType<Tilemap>();
+    }
+    
+    public Vector3 GetCellWorldCenterPosition(Vector3Int cellPosition)
+    {
+        return tilemap.GetCellCenterWorld(cellPosition);
+    }
+    public Vector3Int GetCellAtWorldPosition(Vector3 worldPosition)
+    {
+        return tilemap.WorldToCell(worldPosition);
+    }
+    public bool IsCellWalkable(Vector3Int cellPosition)
+    {
+        //TBD
+        return true;
+    }
+
+    public List<Vector3Int> GetNearCells(Vector3Int cellPosition, int distance)
+    {
+        if (distance < 1) return new List<Vector3Int>();
+        if (distance == 1)
+        {
+            var result = new List<Vector3Int>();
+            AddIfWalkable(result, cellPosition, -1, 0);
+            AddIfWalkable(result, cellPosition, 1, 0);
+            if (cellPosition.y % 2 == 0)
+            {
+                AddIfWalkable(result, cellPosition, -1, 1);
+                AddIfWalkable(result, cellPosition, -1, -1);
+                AddIfWalkable(result, cellPosition, 0, 1);
+                AddIfWalkable(result, cellPosition, 0, -1);
+            }
+            else
+            {
+                AddIfWalkable(result, cellPosition, 0, 1);
+                AddIfWalkable(result, cellPosition, 0, -1);
+                AddIfWalkable(result, cellPosition, 1, 1);
+                AddIfWalkable(result, cellPosition, 1, -1);
+            }
+            return result;
+        }
+        
+        HashSet<Vector3Int> added = new HashSet<Vector3Int>();
+        List<Vector3Int> fringe = new List<Vector3Int>();
+        fringe.Add(cellPosition);
+        added.Add(cellPosition);
+        for (int i = 0; i < distance; i++)
+        {
+            var next = new List<Vector3Int>();
+            foreach (var cell in fringe)
+            {
+                foreach (var near in GetNearCells(cell, 1))
+                {
+                    if (!added.Contains(near))
+                    {
+                        next.Add(near);
+                        added.Add(near);
+                    }
+                } 
+            }
+
+            fringe = next;
+        }
+
+        added.Remove(cellPosition);
+        return new List<Vector3Int>(added);
+    }
+
+    public List<Vector3> GetNearCellsWorldPoints(Vector3Int cellPosition, int distance)
+    {
+        var cells = GetNearCells(cellPosition, distance);
+        var points = new List<Vector3>();
+        foreach (var cell in cells)
+        {
+            points.Add(GetCellWorldCenterPosition(cell));
+        }
+        return points;
+    }
+
+    private void AddIfWalkable(List<Vector3Int> list, Vector3Int cellPosition, int deltax, int deltay)
+    {
+        var cell = new Vector3Int(cellPosition.x + deltax, cellPosition.y + deltay, cellPosition.z);
+        if (IsCellWalkable(cell))
+            list.Add(cell);
+    }
+}
