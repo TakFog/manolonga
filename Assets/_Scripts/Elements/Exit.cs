8d8434aba087f201a193ea3c2905cf2f9fe2846d
Francesco Esposito <francesco7.esposito@gmail.com>
2025-01-26 12:53:29 +0100

    Tiles update

--- Assets/_Scripts/Elements/Exit.cs
+++ Assets/_Scripts/Elements/Exit.cs
@@ -2,8 +2,10 @@
 
 public class Exit : MonoBehaviour
 {
+    [SerializeField] private Sprite exitClosed;
     public void CloseExit()
     {
+        GetComponent<SpriteRenderer>().sprite = exitClosed;
         ExitManager.Instance.CloseExit(this);
     }
 }
