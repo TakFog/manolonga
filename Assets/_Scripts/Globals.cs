218f321924a8256e9e75a4d67cdc68d6aee280c4
tibertisimone94@gmail.com <tibertisimone94@gmail.com>
2025-01-29 16:31:38 +0100

    add changes from my pc

--- Assets/_Scripts/Globals.cs
+++ Assets/_Scripts/Globals.cs
@@ -6,7 +6,7 @@ public class Globals
     public static PlayerType PlayerType { get; set; }
     public static Child Child;
     public static Monster Monster;
-    public static string ServerAddress = "localhost:8080";
+    public static string ServerAddress = "https://ffa2-109-55-143-128.ngrok-free.app";
     
     public static GameObject Player
     {
